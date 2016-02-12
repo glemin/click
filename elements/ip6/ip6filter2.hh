d364028c56f109721cb3852a5c4bf6513dd04973
glemin <xxx@xxx.xx>
2016-02-12 05:34:54 +0100

    backup

--- elements/ip6/ip6filter2.hh
+++ elements/ip6/ip6filter2.hh
@@ -112,6 +112,22 @@ classifier pattern.
 IP6Classifier, Classifier, CheckIP6Header, MarkIP6Header, 
 AddressInfo, tcpdump(1) */
 
+enum {  /* temporarily */
+    // if you change this, change click-fastclassifier.cc also
+    offset_mac = 0,
+    offset_net = 256,       
+    offset_transp = 512,
+
+    // fictive offset values of IPv6 extension headers
+    offset_hop_by_hop = 700,    // kunnen ook iets andes kiezen, en telkens elk ander type IPv6 offset 100 verder laten beginnen
+    offset_destination_options = 800,
+    offset_routing = 900,
+    offset_fragment = 1000,
+    offset_authentication_header = 1100,
+    offset_encapsulating_security_payload = 1200,
+    offset_mobility = 1300
+};
+
 class IP6Filter : public Element { public:
 
     IP6Filter() CLICK_COLD;     // CLICK_COLD is an indication to the compiler that this function is rarely used.
@@ -122,7 +138,7 @@ class IP6Filter : public Element { public:
     const char *processing() const		{ return PUSH; }
     // this element does not need AlignmentInfo; override Classifier's "A" flag
     const char *flags() const			{ return ""; }              // TODO We hebben schijnbaar geen flags. Wat is eigenlijk zo'n flag en waarvoor dient het?
-    bool can_live_reconfigure() const		{ return true; }        // TODO Wat betekent deze functie? Wat houdt live herconfigureren in?
+    bool can_live_reconfigure() const	{ return true; }            // TODO Wat betekent deze functie? Wat houdt live herconfigureren in?
 
     int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;     // CLICK_COLD is an indication to the compiler that this function is rarely used.
     void add_handlers() CLICK_COLD;                                 // CLICK_COLD is an indication to the compiler that this function is rarely used.
@@ -174,50 +190,13 @@ private:
     // is looking something up in the global database of NameInfo, I suppose..........
     static int lookup(String word, int type, int transp_proto, uint32_t &data, const Element *context, ErrorHandler *errh); // dees zoekt iet op, wa is oek nog ni zu duidelijk
 
-    struct Parser { // ne parser zeker, mor wat diej doe is nog ni zu duidelijk
-	    const Vector<String> &_words; //  iet van woorde, wa ne const vector just is, weet ek oek ni direct..., wil da zegge da eens da ge diej initiatlized da er geen elementn ni meer kunne toegeveogd worre of is da toch iet anders..
-	    Vector<int> &_tree; // ne vagen boom der oek nog tusse, deze keer blijkbaar ni const
-	    Classification::Wordwise::Program &_program;       // dit is diej low level brol van hierboven!! das hetzelfde als _zprog !!
-	    const Element *_context;        // een element wordt hier een context genoemd => not sure why :d
-	    ErrorHandler *_errh;
-	    Primitive _prev_prim;           // Waarom hebben we die prev prim nodig?????? EEEn wat happens when it is the first, is this then 0???? or so oo mthing eeelsee ..
-
-	    Parser(const Vector<String> &words, Vector<int> &tree,  /* constructor does set all variables with : syntax, does nothing else */
-	        Classification::Wordwise::Program &program,
-	        const Element *context, ErrorHandler *errh)
-	    : _words(words), _tree(tree), _program(program), _context(context),       // here we set all those variables
-	      _errh(errh) { }
-
-        struct ParseState {                  /* ParseState is a struct inside a struct */
-            int stateNumber;
-    	    int lastPosition;
-    	    ParseState(int givenStateNumber) : stateNumber(givenStateNumber) { } /* constructor does set state variable with : syntax, does nothing else */
-    	};
-    	enum {      // s stands for state, or for statement
-    	    s_expr0, s_expr1, s_expr2,          // These names are being used to give a name to all states in the push-down automata.
-    	    s_orexpr0, s_orexpr1,               // As well as being used as Stack symbols that can be pushed on the stack or consumed.
-    	    s_term0, s_term1, s_term2,          // If the stack empties, we arrive in a final state and the parsing has ended.
-	        s_factor0, s_factor1, s_factor2,    // For more information on this topic view "Introduction to automata theory, languages and computation".
-    	    s_factor0_neg, s_factor1_neg, s_factor2_neg // A book written by Hopcroft, Motwani and Ullman.
-    	};
-
-    	void parse_slot(int output, int pos);   // momenteel ongebruikt maar de functionaliteit zit in parse_program
-    	int parse_expr_iterative(int pos);
-    	int parse_primitive(int pos, bool negated);  // functie die de functionaliteit bevat om tests te parsen, een test is: true, false, quals data of quals relop data
-    };
-
     static int length_checked_match(const IPFilterProgram &zprog, const Packet *p, int packet_length);
 
     static String program_string(Element *e, void *user_data); // wtf is dit? is dit een string versie van euh... u hele klasse ofzu??? mor iet mottig precies!!!
 };
 
-// vanaf hier volgen heel wat inline declaraties, die traditionel in uw .hh staan, i.p.v. uw .cc, mor 't zijn wel implementaties!
-inline bool
-IP6Filter::Primitive::has_transp_proto() const      // waarde groter dan nul betekent dat er een transport protocol aanwezig is
-{
-    return _transp_proto >= 0;          // TODO so if you want to let them know that there is no trasport protocol set in this Wireshark Primitive, then you need to insert a negative value.
-}
 
+/*
 inline bool
 IP6Filter::Primitive::negation_is_simple() const    // vaag
 {
@@ -228,7 +207,7 @@ IP6Filter::Primitive::negation_is_simple() const    // vaag
     else
 	    return _internalPrimitiveType == TYPE_HOST || (_internalPrimitiveType & TYPE_FIELD) || _internalPrimitiveType == TYPE_IPFRAG;      // if _internalPrimitiveType is equal to one of those 3 return true, otherwise return false (then we have an unknown _internalPrimitiveType I guess), you need to try them all 3 out if needed
 }
-
+*/
 
 // TODO
 // TODO     Hier gaat het zprog de in zichzelf opgeslagen zprog boom op de proef gesteld worden
@@ -347,76 +326,7 @@ IP6Filter::match(const IPFilterProgram &zprog, const Packet *p)     // wat bevat
     }
 }
 
-class Primitive {
-public:     
 
-    // I guess this might be unparsed data, that later on will be moved to some of the parsed data fields as _u, _transp_proto or _ip6Address.
-
-    // op and opNegated is a pair of variables that belong together, although they are not abstrated away in a pair.
-
-    // srcDst is a member that tells whether the SRCORDST is set, and if it's set what its value is.
-    // [SRCORDST] host IPADDR
-    // [SRCORDST] net NETADDR
-    // [SRCORDST] [tcp | udp] port PORT
-    // [SRCORDST] ether host ETH
-                                
-    // Remark that when SRCORDST is not given, they will typically let it be equal to 'src or dst'.
-
-
-    // TODO this field is maybe the one that you use when a stand-alone 'proto'-keyword was used in a (Wireshark)Primitive expression?
-    // TODO  Orrrrrrr..... maybe this field is just used for any reference to a protocol number, since a "normal" _proto is appearing nowhere. This IS just the protocol reference. Maybe we should rename it to "protcol" since "_transp_proto" doesn't seem to make much sense because , if I am right, this can also be something as ICMP or IGMP which aren't in fact transport protocols.
-    // dat zal het nummer van het transport protocol zal, zal wel toegekend worden op basis van 1 van die CONSTANTEN bovenaan denk ik he
-
-    /* om dingen op te slaan als het IPv4 adres of het Ethernet address => we gaan dat nu allemaal opslaan in afzonderlijke variabelen hier en een request for rewrite schrijven om dit te herschrijven met overerving */
-    // PrimitiveData wordt nu gebruikt voor de opslag van de data voor de Primitive als die data <= 4 bytes.
-    // Indien deze data groter is dan 4 bytes dan moet het wel om een IP6Address gaan, en in dat geval wordt het opgeslagen in het IP6Address veld.
-                                // -> een "probleem" dat we hebben is nu dat, de data nu variabele groottes kan hebben en groter kan zijn dan 4 bytes!
-    // denk maar aan een IPv6 adres, dat is 16 bytes lang
-
-    // De data die gedragen wordt door de primitive zal dus veel groter kunnen zijn dan vroeger het geval was, hoe gaan we hier mee omgaan?                                
-
-    // Dit is het masker van de hier bijbehorende data.
-
-    // hier een masker van een IPv4 adres denk ik*, wat zo'n masker ook moge zijn
-    // * is niet enkel van een IPv4 adres maar ook van andere zaken, bv. het IPCE veldje, wat dan maar een masker gebruikt dat 2 bits in beslag neemt i.p.v. een hele resem van de 32 bits, of in het geval van een IPv4 adres zelfs alle bits. Bij een IPv6 adres is het zelfs te lang en ga je in de instructietaal 4 velden van 32-bit met een AND-operator moeten verbinden.
-
-
-    // As for if type is HOST, NETADDR, ... (netaddr vermoed ik ook -> opzoeken  ; eventueel extra velden maken voor EtherAddress enzo => en dan gently vragen of het kan omgezet worden naar een versie die werkt met inheritance.)
-
-    Primitive()			{ clear(); }    // eerst alles clearen voor je een nieuwe primitive maakt, er zal misschien ook wel wat opgeslagen worden in de Primitive class zelf
-
-    void clear();                       // hier dus alles wissen dat nog is de klasse zat
-
-
-
-
-
-    void print();
-
-
-
-
-
-
-
-
-    int set_mask(uint32_t full_mask, int shift, uint32_t provided_mask, ErrorHandler *errh); // hiermee zet je dat masker, wat dat ook moge zijn
-    int check(const Primitive &prev_prim, int level, int mask_dt, const PrimitiveData &mask, ErrorHandler *errh); // hiermee check je iets, we weten niet nog wat
-
-    void compile(Classification::Wordwise::Program &p, Vector<int> &tree) const; // => dit vertaalt de primitive naar een low level constructie die verwerkt kan worden door een 'gewone' classifier
-
-
-
-
-    bool negation_is_simple() const; // nog niet echt duidelijk, we zullen nog moeten bekijken wat ze daarmee willen zeggen
-    void simple_negate(); // ja en dit is dan misschien iets dat ge kunt doen als de negatie simpel is, wat het ook wille zeggen
-
-
-private:
-    int type_error(ErrorHandler *errh, const char *msg) const; // voor als er iets misloept ofzu, ni echt duideijk mor ik veronderstel dat da dan via ErrorHandler en msg wa twee pointers zijn, wa meer info over diej error teruggeeft, das een alternaief denk ik voor het gebruik van throws enzu
-    void add_comparison_exprs(Classification::Wordwise::Program &p, Vector<int> &tree, int offset, int shift, bool swapped, bool op_negate) const; // dees is echt nog sjakka makka
-
-};
 
 CLICK_ENDDECLS
 #endif
